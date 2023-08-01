e0c522cedf07db992dd4e4a3c53cd5e7c26835e6
谁书-ss <yushuishu@qq.com>
2023-08-01 22:09:56 +0800

    feat:配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { homepage } from './package.json';
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({command, mode}) => {
+    const root = process.cwd();
+    const env = loadEnv(mode, root);
+    console.log('env', env);
+    return {
+        base: mode === 'build' ? homepage : '/',
+        publicDir: './public',
+        server: {
+            proxy: {
+                'dev': {
+                    target: env.VITE_BASE_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/dev/, ''),
+                },
+                '/socket.io': {
+                    target: 'ws://localhost:5174',
+                    ws: true,
+                },
+            },
+        },
+        resolve: {
+            alias: {
+                // 键必须以斜线开始和结束
+                '@': path.resolve(__dirname, './src')
+            }
+        },
+        plugins: [
+            vue(),
+            // ElementPlus({
+            //   useSource: true
+            // })
+        ]
+    };
 })
